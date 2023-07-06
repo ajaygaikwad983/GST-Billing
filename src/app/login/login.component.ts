74d5840bd4ce4d9c9c9a611a4d0ba81d2d355a56
ajay <ajaygaikwad983@gmail.com>
2023-07-06 11:12:34 +0530

    Application electron conversion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,31 +5,33 @@ import { LoginService } from '../login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   public record;
 
-  constructor(private loginService: LoginService, private route: Router) { }
+  constructor(private loginService: LoginService, private route: Router) {}
 
   public notValid = false;
+  public error = '';
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(form) {
     this.loginService.getUser().subscribe(
       (listBill) => {
-        if (listBill[0].name === form.txtUser && listBill[0].password === form.txtPassword) {
+        this.error = '';
+        if (
+          listBill[0].name === form.txtUser &&
+          listBill[0].password === form.txtPassword
+        ) {
           sessionStorage.setItem('token', listBill[0].id);
           this.route.navigate(['/billing']);
         } else {
           this.notValid = true;
         }
       },
-      (err) => console.log(err)
+      (err) => (this.error = err)
     );
   }
-
 }
